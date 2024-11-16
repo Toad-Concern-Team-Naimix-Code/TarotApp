301c86a5486d4df977abdee4b2030acc64417566
Egor Overchenko <e.overchenko@uno-soft.ru>
2024-11-16 14:03:36 +0300

    Created sign up screen

--- app/src/main/java/com/example/tarotapp/view/fragments/SecondFragment.kt
+++ app/src/main/java/com/example/tarotapp/view/fragments/SecondFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.navigation.fragment.findNavController
 import com.example.tarotapp.R
 import com.example.tarotapp.databinding.FragmentSecondBinding
@@ -22,12 +23,18 @@ class SecondFragment : Fragment() {
 
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val companies = resources.getStringArray(R.array.companies)
+        val companiesAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_intem, companies)
+        binding.spinnerCompany.setAdapter(companiesAdapter)
 
+
+        val departments = resources.getStringArray(R.array.departments)
+        val departmentsAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_intem, departments)
+        binding.spinnerDepartment.setAdapter(departmentsAdapter)
     }
 
     override fun onDestroyView() {
